a117b2625ba5269be0ba2ab3fc3386188c16d669
SousaUxp <jsousa.uxp@gmail.com>
2022-05-03 15:50:51 +0100

    deployed to azure

--- src/assets/Api.ts
+++ src/assets/Api.ts
@@ -1,1 +1,2 @@
-export const ApiLink = 'http://localhost:57316/api'
+// export const ApiLink = 'http://localhost:57316/api'
+export const ApiLink = 'https://webapplication120220503151115.azurewebsites.net/api'
